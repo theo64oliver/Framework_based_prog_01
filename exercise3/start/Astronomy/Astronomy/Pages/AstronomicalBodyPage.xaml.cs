934461443e640c798626458e79ce754775623833
theo <theo.oliver@epita.fr>
2023-03-02 21:04:27 +0700

    first

---
+++ exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodyPage.xaml.cs
@@ -0,0 +1,48 @@
+namespace Astronomy.Pages;
+
+public partial class AstronomicalBodyPage : ContentPage
+{
+
+    public AstronomicalBodyPage()
+    {
+        InitializeComponent();
+    }
+
+    void UpdateAstroBodyUI(string astroName)
+    {
+        AstronomicalBody body = FindAstroData(astroName);
+
+        Title = body.Name;
+
+        lblIcon.Text = body.EmojiIcon;
+        lblName.Text = body.Name;
+        lblMass.Text = body.Mass;
+        lblCircumference.Text = body.Circumference;
+        lblAge.Text = body.Age;
+    }
+
+    string astroName;
+    public string AstroName
+    {
+        get => astroName;
+        set
+        {
+            astroName = value;
+
+            // this is a custom function to update the UI immediately
+            UpdateAstroBodyUI(astroName);
+        }
+    }
+
+    AstronomicalBody FindAstroData(string astronomicalBodyName)
+    {
+        return astronomicalBodyName switch
+        {
+            "comet" => SolarSystemData.HalleysComet,
+            "earth" => SolarSystemData.Earth,
+            "moon" => SolarSystemData.Moon,
+            "sun" => SolarSystemData.Sun,
+            _ => throw new ArgumentException()
+        };
+    }
+}
